48cc838eb993b196c0bd47c9a8dd0471b6bede67
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-02 16:56:22 -0300

    Update VideoState.hx

--- source/VideoState.hx
+++ source/VideoState.hx
@@ -49,7 +49,7 @@ class VideoState extends MusicBeatState
 		//FlxG.autoPause = true;
 		trace('close!');
 		trace(nextState);
-		MusicBeatState.switchState(nextState);
+		MusicBeatState(nextState);
 	}
 
 	function onURLChanging(url:String) {
