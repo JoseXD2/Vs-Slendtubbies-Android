57c607a0dd33da804f907e7878ee360f7827a70f
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-02 16:52:41 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -370,6 +370,7 @@ class StoryMenuState extends MusicBeatState
 			PlayState.campaignScore = 0;
 			new FlxTimer().start(1, function(tmr:FlxTimer)
 			{            
+				LoadingState.loadAndSwitchState(new VideoState('assets/videos/cutscenetw1', new StoryMenuState())); 
 				LoadingState.loadAndSwitchState(new PlayState(), true);
 			});
 		}
